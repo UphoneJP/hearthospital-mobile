5572adce15be6935d907d6beea49d2a6a1217d76
hearthospital <support@hearthospital.js>
2025-03-10 10:23:06 +0900

    コンフリクト解消

--- src/app/user/points.tsx
+++ src/app/user/points.tsx
@@ -1,31 +1,6 @@
 import { ScrollView, View } from "react-native"
 import BackgroundTemplate from "@/src/components/template/BackgroundTemplete"
 import BannerAds from "@/src/components/template/BannerAds"
-<<<<<<< HEAD
-import PointsGet from "@/src/components/points/PointsGet"
-import PointsUse from "@/src/components/points/PointsUse"
-import PointsHeader from "@/src/components/points/PointsHeader"
-
-export default function Points() {
-  return (
-    <BackgroundTemplate>
-      <ScrollView style={{width: '100%'}}>
-        
-        <PointsHeader />
-
-        <PointsGet />
-        
-        <PointsUse />          
-
-        <View style={{padding: 64}}/>
-      </ScrollView>
-
-      <View style={{position: 'absolute', bottom: 0}}>
-        <BannerAds />
-      </View>
-
-    </BackgroundTemplate>
-=======
 import PointsHeader from "@/src/components/points/PointsHeader"
 import PointsGet from "@/src/components/points/PointsGet"
 import PointsUse from "@/src/components/points/PointsUse"
@@ -50,6 +25,5 @@ export default function Points() {
         </View>
 
       </BackgroundTemplate>
->>>>>>> edf4333 (InterstitialAdsによりアプリがクラッシュしたため、再度作り直した。)
   )
 }
