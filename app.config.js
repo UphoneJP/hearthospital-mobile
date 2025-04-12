f92f234d8e30fbe223f39849fbff0d2eb498680d
hearthospital <support@hearthospital.js>
2025-04-12 10:19:02 +0900

    versionの名前変更

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default{
     name: "hearthospital-mobile",
     slug: "hearthospital-mobile",
     scheme: "jp.hearthospital",
-    version: "1.1.5",
+    version: "1.1.6",
     orientation: "portrait",
     icon: "./assets/icon(1024x1024).png",
     splash: {
@@ -15,7 +15,7 @@ export default{
     newArchEnabled: true,
     ios: {
       bundleIdentifier: "jp.hearthospital",
-      buildNumber: "1.1.5",
+      buildNumber: "1.1.6",
       supportsTablet: false,
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAP_API_KEY_FOR_IOS
@@ -27,7 +27,7 @@ export default{
     },
     android: {
       package: "jp.hearthospital",
-      versionCode: 15,
+      versionCode: 16,
       permissions: [],
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive(432x432).png",
